f80aaab15bc74846c3534d208436317110ac2952
Manish Kumar Sah <47948821+manishkumarsahgopsaheb@users.noreply.github.com>
2020-10-08 13:33:22 +0530

    second commit

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ window = Tk()
 
 
 def getvals():
+#     getting value
     getname = namevalue.get()
     getphone = phonevalue.get()
     getgender = gendervalue.get()
@@ -11,11 +12,15 @@ def getvals():
     getpaymentmode = payment_modevalue.get()
     getfoodservice = foodservicevalue.get()
 
+#     writing or storing all the values in a txt file
+
     with open("1.txt", "a") as file:
         file.write(f"{getname}, {getphone}, {getgender}, {getemergency}, {getpaymentmode}, {getfoodservice}\n")
 
 
 window.geometry("644x344")
+
+# creating all the lables
 
 Label(text="welcome to Manish Travels", font="comicsanms 13 bold").grid(row=0, column=3)
 
@@ -25,18 +30,24 @@ gender = Label(window, text="Gender")
 emergency = Label(window, text="Emergency")
 payment_mode = Label(window, text="Payment Mode")
 
+# griding all the labels
+
 name.grid(row=1, column=2)
 phone.grid(row=2, column=2)
 gender.grid(row=3, column=2)
 emergency.grid(row=4, column=2)
 payment_mode.grid(row=5, column=2)
 
+# declaring tkinter variables for accepting the values for user
+
 namevalue = StringVar()
 phonevalue = StringVar()
 gendervalue = StringVar()
 emergencyvalue = StringVar()
 payment_modevalue = StringVar()
-foodservicevalue = IntVar()  # this is for the check box and check box return either 0 or 1
+foodservicevalue = IntVar()  # this variable is for the check box and check box return either 0 or 1
+
+# creating of entry for all the labels
 
 name_entry = Entry(window, textvariable=namevalue)
 phone_entry = Entry(window, textvariable=phonevalue)
@@ -44,16 +55,20 @@ gender_entry = Entry(window, textvariable=gendervalue)
 emergency_entry = Entry(window, textvariable=emergencyvalue)
 payment_mode_entry = Entry(window, textvariable=payment_modevalue)
 
+# griding of all entries
+
 name_entry.grid(row=1, column=3)
 phone_entry.grid(row=2, column=3)
 gender_entry.grid(row=3, column=3)
 emergency_entry.grid(row=4, column=3)
 payment_mode_entry.grid(row=5, column=3)
 
-# lets create a checkbox
+# lets create a checkbox and grid it
 
 foodservice = Checkbutton(window, text="want to prebook your meal?", variable=foodservicevalue)
 foodservice.grid(row=6, column=3)
+
+# creation of button and griding it
 
 Button(window, text="Submit", command=getvals).grid(row=7, column=3)
 
